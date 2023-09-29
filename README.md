5491287102fa042ffe0fceb54f88a7421d2b7fad
LpgLevin <lpglevin@gmail.com>
2023-09-29 13:21:25 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Medi-Score-Calculation
-This is an function which calculates a patient's medi score. The score is based on biological observation readings for different categories. Those readings translate into scores, which can be added up to their overall score.
+This is an function which calculates a patient's medi score. 
+The score is based on biological observation readings for different categories. 
+Those readings translate into scores, which can be added up to their overall score.
+
+# 
+
---
+++ data/patient-objects.js
@@ -0,0 +1,25 @@
+const patients = [
+    {
+        p1: {
+                measurements: {
+                                air_or_oxygen: 0, 
+                                consciousness: 0,
+                                respiration_range: 15,
+                                spo2: 95,
+                                temperature: 37.1
+                },
+
+	            scores: {
+                            air_or_oxygen: 0,
+                            consciousness: 0,
+                            respiration_range: 0,
+                            spo2: 0,
+                            temperature: 0
+                },
+
+	            total_score : 0
+
+        }
+    }
+
+]
